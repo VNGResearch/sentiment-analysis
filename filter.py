3aad2b09760ff5feb42afe392e10d851f30bb0c5
= <phan.ngoclan58@gmail.com>
2017-09-12 13:29:30 +0700

    Add params to filter

--- filter.py
+++ filter.py
@@ -1,14 +1,27 @@
+from argparse import ArgumentParser
+
 from sentiment.wordfilter import UnivariateFilter
 from sentiment.utils import load_data
 
 
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('--data', default='data/amazon/train.tsv', dest='DATA')
+    parser.add_argument('--size', default=20000, type=int, dest='SIZE')
+    parser.add_argument('--no-below', default=10, type=int, dest='BELOW')
+    parser.add_argument('--no-above', default=0.99, type=float, dest='ABOVE')
+    parser.add_argument('-k', default=30, type=int, dest='K')
+
+    return parser.parse_args()
+
+
 def main(args):
-    docs, labels = load_data('data/amazon/train.tsv')
+    docs, labels = load_data(args.DATA)
     mfilter = UnivariateFilter(docs, labels, stopwords='english',
-                               vocab_size=20000, no_below=10, no_above=0.95)
-    mfilter.fit(k=100)
+                               vocab_size=args.SIZE, no_below=args.BELOW, no_above=args.ABOVE)
+    mfilter.fit(k=args.K)
     mfilter.save_filter()
 
 
 if __name__ == '__main__':
-    main(None)
+    main(parse_arguments())
